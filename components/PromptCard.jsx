9d25b42a153ead37d98bc75493908c274ac9fe2a
3vdi <unitcomics.world@proton.me>
2023-05-25 20:36:40 +0530

    feed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -34,31 +34,23 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
           onClick={handleProfileClick}
         >
           <Image
-            src={post.creator.image}
+            src="/robot.png"
             alt='user_image'
             width={40}
             height={40}
             className='rounded-full object-contain'
           />
 
-          <div className='flex flex-col'>
-            <h3 className='font-satoshi font-semibold text-gray-900'>
-              {post.creator.username}
-            </h3>
-            <p className='font-inter text-sm text-gray-500'>
-              {post.creator.email}
-            </p>
-          </div>
         </div>
 
         <div className='copy_btn' onClick={handleCopy}>
           <Image
             src={
               copied === post.prompt
-                ? "/assets/icons/tick.svg"
-                : "/assets/icons/copy.svg"
+                ? "/robot.png"
+                : "/robot.png"
             }
-            alt={copied === post.prompt ? "tick_icon" : "copy_icon"}
+            alt={copied === post.prompt ? "robot.png" : "robot.png"}
             width={12}
             height={12}
           />
@@ -73,22 +65,6 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
         #{post.tag}
       </p>
 
-      {session?.user.id === post.creator._id && pathName === "/profile" && (
-        <div className='mt-5 flex-center gap-4 border-t border-gray-100 pt-3'>
-          <p
-            className='font-inter text-sm green_gradient cursor-pointer'
-            onClick={handleEdit}
-          >
-            Edit
-          </p>
-          <p
-            className='font-inter text-sm orange_gradient cursor-pointer'
-            onClick={handleDelete}
-          >
-            Delete
-          </p>
-        </div>
-      )}
     </div>
   );
 };
